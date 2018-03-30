8599c71f324067e88240ceaa19c725c73d918602
Woopa01 <whdnwls49@naver.com>
2018-03-31 07:59:41 +0900

    fifth commit

---
+++ app/src/main/java/com/dsm2017/example/gcp_hack/AndroidActivity.java
@@ -0,0 +1,17 @@
+package com.dsm2017.example.gcp_hack;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dsm2017 on 2018-03-31.
+ */
+
+public class AndroidActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_android);
+    }
+}
