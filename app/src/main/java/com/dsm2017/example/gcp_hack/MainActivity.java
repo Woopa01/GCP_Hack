4e2f8824366a432b89b4bcf92a22d6293970cac3
Woopa01 <whdnwls49@naver.com>
2018-03-31 05:15:05 +0900

    first commit

---
+++ app/src/main/java/com/dsm2017/example/gcp_hack/MainActivity.java
@@ -0,0 +1,14 @@
+package com.dsm2017.example.gcp_hack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // asdfasdfasdfasdfasdf
+    }
+}
